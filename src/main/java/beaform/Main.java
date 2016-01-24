dbfc58ab92615bd6a2910752eabb4aa0f00d5624
Steven Post <redalert.commander@gmail.com>
2016-01-24 17:20:20 +0100

    Add some sample Neo4j code

---
+++ src/main/java/beaform/Main.java
@@ -0,0 +1,65 @@
+package beaform;
+
+import java.io.File;
+
+import org.neo4j.graphdb.Direction;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
+public class Main {
+
+	public static void main(String[] args) {
+		final GraphDatabaseService graphDb;
+		Node firstNode;
+		Node secondNode;
+		Relationship relationship;
+
+		graphDb = new GraphDatabaseFactory().newEmbeddedDatabase(new File("neo4j/db"));
+		Runtime.getRuntime().addShutdownHook(new ShutDownHook(graphDb));
+
+		try ( Transaction tx = graphDb.beginTx()) {
+			firstNode = graphDb.createNode();
+			firstNode.setProperty( "message", "Hello, " );
+			secondNode = graphDb.createNode();
+			secondNode.setProperty( "message", "World!" );
+
+			relationship = firstNode.createRelationshipTo( secondNode, RelTypes.CONTAINS );
+			relationship.setProperty( "message", "brave Neo4j " );
+
+			tx.success();
+		}
+
+		try ( Transaction tx = graphDb.beginTx()) {
+			System.out.print( firstNode.getProperty( "message" ) );
+			System.out.print( relationship.getProperty( "message" ) );
+			System.out.print( secondNode.getProperty( "message" ) );
+		}
+
+		try ( Transaction tx = graphDb.beginTx()) {
+			// let's remove the data
+			firstNode.getSingleRelationship( RelTypes.CONTAINS, Direction.OUTGOING ).delete();
+			firstNode.delete();
+			secondNode.delete();
+			tx.success();
+		}
+
+	}
+
+
+	private static class ShutDownHook extends Thread {
+
+		private final GraphDatabaseService graphDb;
+
+		public ShutDownHook(GraphDatabaseService graphDb) {
+			this.graphDb = graphDb;
+		}
+
+		@Override
+		public void run() {
+			this.graphDb.shutdown();
+		}
+	}
+}
---
+++ src/main/java/beaform/RelTypes.java
@@ -0,0 +1,7 @@
+package beaform;
+
+import org.neo4j.graphdb.RelationshipType;
+
+public enum RelTypes implements RelationshipType {
+	CONTAINS,
+}
