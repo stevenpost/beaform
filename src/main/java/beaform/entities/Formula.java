f344bbcc4d44f5b6cb9cb9be2db1b342dd51fc86
Steven Post <redalert.commander@gmail.com>
2016-04-03 10:40:27 +0200

    Make Formulas able to contain other Formulas

--- src/main/java/beaform/entities/Formula.java
+++ src/main/java/beaform/entities/Formula.java
@@ -1,8 +1,14 @@
 package beaform.entities;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.GenericGenerator;
 
@@ -15,6 +21,9 @@ public class Formula {
 	private String id;
 	private String name;
 	private String description;
+
+	@OneToMany
+	private final Map<String, Formula> ingredients = new HashMap<String, Formula>();
 
 	public String getId() {
 		return this.id;
@@ -38,6 +47,14 @@ public class Formula {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	public void addIngredient(Formula ingredient, String amount) {
+		this.ingredients.put(ingredient.getName() + "|" + amount, ingredient);
+	}
+
+	public Iterator<Entry<String, Formula>> getIngredients() {
+		return this.ingredients.entrySet().iterator();
 	}
 
 	@Override
