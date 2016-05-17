4ddc9fed9c175e12cea6f27b5251583f48020cfd
Steven Post <redalert.commander@gmail.com>
2016-05-17 12:12:24 +0200

    Fix multiple PMD errors

--- src/main/java/beaform/gui/TreeViewFormula.java
+++ src/main/java/beaform/gui/TreeViewFormula.java
@@ -4,34 +4,62 @@ import beaform.Ingredient;
 import beaform.entities.Formula;
 
 /**
- * This class represents a formula for use in a treeview
+ * This class represents a formula for use in a tree view.
  *
- * @author steven
+ * @author Steven Post
  *
  */
 public class TreeViewFormula {
+
+	/** The formula this object represents */
 	private final Formula formula;
+
+	/** The amount of this formula in the total view of the tree */
 	private final String amount;
 
+	/**
+	 * Constructor.
+	 *
+	 * @param formula The formula this object needs to represent
+	 */
 	public TreeViewFormula(final Formula formula) {
 		this.formula = formula;
 		this.amount = "";
 	}
 
+	/**
+	 * Constructor.
+	 *
+	 * @param ingredient The ingredient this object needs to represent
+	 */
 	public TreeViewFormula(final Ingredient ingredient) {
 		this.formula = ingredient.getFormula();
 		this.amount = ingredient.getAmount();
 	}
 
+	/**
+	 * A toString implementation
+	 *
+	 * @return name of the formula this object represents
+	 */
 	@Override
 	public String toString() {
 		return this.formula.getName();
 	}
 
+	/**
+	 * Getter for the formula that this object is representing.
+	 *
+	 * @return The formula this object is representing
+	 */
 	public Formula getFormula() {
 		return this.formula;
 	}
 
+	/**
+	 * Getter for the amount of this formula in the whole.
+	 * @return the amount
+	 */
 	public String getAmount() {
 		return this.amount;
 	}
