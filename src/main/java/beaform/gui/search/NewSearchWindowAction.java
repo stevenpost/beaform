fb5795773bcb1ffcb718c9fa64d90607b154549b
Steven Post <redalert.commander@gmail.com>
2016-05-18 06:39:19 +0200

    Add JavaDoc

--- src/main/java/beaform/gui/search/NewSearchWindowAction.java
+++ src/main/java/beaform/gui/search/NewSearchWindowAction.java
@@ -5,10 +5,21 @@ import java.awt.event.ActionListener;
 
 import beaform.gui.MainGUI;
 
+/**
+ * This action opens up a new search.
+ *
+ * @author Steven Post
+ *
+ */
 public class NewSearchWindowAction implements ActionListener {
 
+	/**
+	 * Invoked when the action occurs.
+	 *
+	 * @param event the event object
+	 */
 	@Override
-	public void actionPerformed(final ActionEvent e) {
+	public void actionPerformed(final ActionEvent event) {
 		MainGUI.getInstance().replaceWindow(new SearchGui());
 	}
 
