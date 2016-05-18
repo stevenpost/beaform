d05a06288a3fadb9e12bf6a1c590815051bc9da0
Steven Post <redalert.commander@gmail.com>
2016-05-18 06:32:20 +0200

    Fix several PMD violations

--- src/main/java/beaform/gui/formulaeditor/DelIngredientAction.java
+++ src/main/java/beaform/gui/formulaeditor/DelIngredientAction.java
@@ -8,20 +8,42 @@ import javax.swing.JList;
 
 import beaform.entities.Ingredient;
 
+/**
+ * This action removes an ingredient from the list of ingredients.
+ *
+ * @author Steven Post
+ *
+ */
 public final class DelIngredientAction implements ActionListener {
 
+	/**
+	 * The list model for ingredients.
+	 */
 	private final DefaultListModel<Ingredient> lstFormulaModel;
+
+	/** The graphical list */
 	private final JList<Ingredient> lstFormulas;
 
-	public DelIngredientAction(JList<Ingredient> lstFormulas, DefaultListModel<Ingredient> lstFormulaModel) {
+	/**
+	 * Constructor
+	 *
+	 * @param lstFormulas The graphical list.
+	 * @param lstFormulaModel The list model for ingredients.
+	 */
+	public DelIngredientAction(final JList<Ingredient> lstFormulas, final DefaultListModel<Ingredient> lstFormulaModel) {
 		this.lstFormulaModel = lstFormulaModel;
 		this.lstFormulas = lstFormulas;
 	}
 
+	/**
+	 * This method is invoked when the event is triggered.
+	 *
+	 * @param event The event object
+	 */
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(final ActionEvent event) {
 		while (!this.lstFormulas.isSelectionEmpty()) {
-			int selected = this.lstFormulas.getSelectedIndex();
+			final int selected = this.lstFormulas.getSelectedIndex();
 			this.lstFormulaModel.remove(selected);
 		}
 	}
