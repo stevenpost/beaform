de02d281d363fa6df1842b2ef740756a7a93dfb3
Steven Post <redalert.commander@gmail.com>
2016-05-18 06:16:30 +0200

    Add JavaDoc

--- src/main/java/beaform/GraphDbHandlerForJTA.java
+++ src/main/java/beaform/GraphDbHandlerForJTA.java
@@ -107,24 +107,36 @@ public class GraphDbHandlerForJTA {
 	}
 
 	/**
-	 * This class is a shutdownhook to make sure the embedded DB is stopped.
+	 * This class is a shutdown hook to make sure the embedded DB is stopped.
 	 *
 	 * @author Steven Post
 	 *
 	 */
 	private final static class ShutDownHook extends Thread {
 
+		/** A logger */
 		private static final Logger LOG = LoggerFactory.getLogger(ShutDownHook.class);
 
+		/** The entity manager factory */
 		private final EntityManagerFactory entityManagerFact;
+
+		/** The entity manager */
 		private final EntityManager entityManager;
 
+		/**
+		 * Constructor.
+		 * @param entityManager The entity manager.
+		 * @param entityManagerFact The entity manager factory.
+		 */
 		public ShutDownHook(final EntityManager entityManager, final EntityManagerFactory entityManagerFact) {
 			super();
 			this.entityManager = entityManager;
 			this.entityManagerFact = entityManagerFact;
 		}
 
+		/**
+		 * Invoked when the hook executes.
+		 */
 		@Override
 		public void run() {
 			LOG.info("Start DB shutdown");
