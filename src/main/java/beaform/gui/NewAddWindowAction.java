0dc2fca7e031a95a3d37a81d1b5d4abaccd5fd6e
Steven Post <redalert.commander@gmail.com>
2016-05-18 06:46:07 +0200

    Add JavaDoc

--- src/main/java/beaform/gui/NewAddWindowAction.java
+++ src/main/java/beaform/gui/NewAddWindowAction.java
@@ -5,10 +5,21 @@ import java.awt.event.ActionListener;
 
 import beaform.gui.formulaeditor.FormulaEditor;
 
+/**
+ * An action to show a clean formula editor, in order to create a new formula.
+ *
+ * @author Steven Post
+ *
+ */
 public class NewAddWindowAction implements ActionListener {
 
+	/**
+	 * Invoked when the action occurs.
+	 *
+	 * @param event the event object
+	 */
 	@Override
-	public void actionPerformed(final ActionEvent e) {
+	public void actionPerformed(final ActionEvent event) {
 		MainGUI.getInstance().replaceWindow(new FormulaEditor());
 	}
 
