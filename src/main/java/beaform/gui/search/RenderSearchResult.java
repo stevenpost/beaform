f905ae4e7142a6737eb8ab0fdfd17beebcb46fc1
Steven Post <redalert.commander@gmail.com>
2016-05-18 05:58:25 +0200

    Add JavaDoc

--- src/main/java/beaform/gui/search/RenderSearchResult.java
+++ src/main/java/beaform/gui/search/RenderSearchResult.java
@@ -12,18 +12,36 @@ import org.slf4j.LoggerFactory;
 import beaform.entities.Formula;
 import beaform.gui.FormulaTree;
 
-public final class RenderSearchResult implements Runnable { // NOPMD by steven on 5/16/16 4:31 PM
+/**
+ * This class is used to render the result of a formula search in the GUI.
+ *
+ * @author Steven Post
+ *
+ */
+public final class RenderSearchResult implements Runnable {
 
 	/** A logger */
 	private static final Logger LOG = LoggerFactory.getLogger(RenderSearchResult.class);
+
+	/** The task with the search result */
 	private final transient Future<Formula> searchresult;
+
+	/** The target panel */
 	private final transient JPanel pane;
 
+	/**
+	 * Constructor.
+	 * @param searchresult The task for the search.
+	 * @param pane The target panel.
+	 */
 	public RenderSearchResult(final Future<Formula> searchresult, final JPanel pane) {
 		this.searchresult = searchresult;
 		this.pane = pane;
 	}
 
+	/**
+	 * Invoked when the action occurs.
+	 */
 	@Override
 	public void run() {
 		try {
@@ -36,18 +54,36 @@ public final class RenderSearchResult implements Runnable { // NOPMD by steven o
 		}
 	}
 
-	public static final class AddFormTreeToGui implements Runnable { // NOPMD by steven on 5/16/16 4:30 PM
+	/**
+	 * A task to to the actual rendering.
+	 *
+	 * @author Steven Post
+	 *
+	 */
+	public static final class AddFormTreeToGui implements Runnable {
+
 		/** The index of the formula tree on the target panel */
 		private static final int FORMULA_TREE_LOC = 3;
 
+		/** The result of the search */
 		private final Formula searchResult;
+
+		/** The target panel */
 		private final JPanel pane;
 
+		/**
+		 * Constructor.
+		 * @param searchResult the result of the search
+		 * @param pane the target panel
+		 */
 		public AddFormTreeToGui(final Formula searchResult, final JPanel pane) {
 			this.searchResult = searchResult;
 			this.pane = pane;
 		}
 
+		/**
+		 * Invoked when the action occurs.
+		 */
 		@Override
 		public void run() {
 			final FormulaTree formulaTree = new FormulaTree(this.searchResult);
