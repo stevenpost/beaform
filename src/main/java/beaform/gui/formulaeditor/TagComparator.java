1d82f63d0e853de7d82d16c9d1596699594c045f
Steven Post <redalert.commander@gmail.com>
2016-05-21 00:26:28 +0200

    Fix several PMD violations

--- src/main/java/beaform/gui/formulaeditor/TagComparator.java
+++ src/main/java/beaform/gui/formulaeditor/TagComparator.java
@@ -13,14 +13,19 @@ import beaform.entities.FormulaTag;
  */
 public class TagComparator implements Comparator<FormulaTag>, Serializable {
 
-	/**
-	 *
-	 */
+	/** A serial */
 	private static final long serialVersionUID = 6949518937805857985L;
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public int compare(final FormulaTag firstTag, final FormulaTag secondTag) {
-		return firstTag.getName().compareTo(secondTag.getName());
+		return compareNames(firstTag.getName(), secondTag.getName());
+	}
+
+	private int compareNames(final String firstString, final String secondString) {
+		return firstString.compareTo(secondString);
 	}
 
 }
