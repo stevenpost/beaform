5ad75cd8f99a74ed5e3ad99327d62c0e60fcab07
Steven Post <redalert.commander@gmail.com>
2016-06-26 13:26:29 +0200

    Add a test for searching by tags

---
+++ src/test/java/beaform/search/SearchFormulasByTagTaskTest.java
@@ -0,0 +1,56 @@
+package beaform.search;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import beaform.dao.GraphDbHandlerForJTA;
+import beaform.debug.DebugUtils;
+import beaform.entities.Formula;
+import junit.framework.TestCase;
+
+/**
+ * Tests for searching formulas by tag.
+ *
+ * @author Steven Post
+ *
+ */
+public class SearchFormulasByTagTaskTest extends TestCase {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@Before
+	public void setUp() {
+		GraphDbHandlerForJTA.initInstance("test");
+		DebugUtils.clearDb();
+		DebugUtils.fillDb();
+	}
+
+	/**
+	 * Test the search method.
+	 * @throws Exception
+	 */
+	@Test
+	public void testSearch() throws Exception {
+		final Callable<List<Formula>> task = new SearchFormulasByTagTask("First");
+		final List<Formula> resultList = task.call();
+		for (final Formula form : resultList) {
+			assertTrue("This formula doesn't match the search criteria", form.getTagsAsStrings().contains("First"));
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@After
+	public void tearDown() {
+		DebugUtils.clearDb();
+	}
+
+}
