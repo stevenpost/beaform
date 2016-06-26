5994cb7712341116350dc3063f4fceb089453085
Steven Post <redalert.commander@gmail.com>
2016-06-26 13:31:57 +0200

    Add test for searching a formula by name

---
+++ src/test/java/beaform/search/SearchFormulasTaskTest.java
@@ -0,0 +1,53 @@
+package beaform.search;
+
+import java.util.concurrent.Callable;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import beaform.dao.GraphDbHandlerForJTA;
+import beaform.debug.DebugUtils;
+import beaform.entities.Formula;
+import junit.framework.TestCase;
+
+/**
+ * Tests for searching formulas by tag.
+ *
+ * @author Steven Post
+ *
+ */
+public class SearchFormulasTaskTest extends TestCase {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@Before
+	public void setUp() {
+		GraphDbHandlerForJTA.initInstance("test");
+		DebugUtils.clearDb();
+		DebugUtils.fillDb();
+	}
+
+	/**
+	 * Test the search method.
+	 * @throws Exception
+	 */
+	@Test
+	public void testSearch() throws Exception {
+		final Callable<Formula> task = new SearchFormulaTask("Form1");
+		final Formula result = task.call();
+		assertEquals("This isn't the tag we searched for", "Form1", result.getName());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	@After
+	public void tearDown() {
+		DebugUtils.clearDb();
+	}
+
+}
