125a61f612a90869a86ef6b86ac05836d223dfd2
Steven Post <steven.post@persgroep.be>
2016-06-30 22:48:40 +0200

    Remove redundant comments

--- src/main/java/beaform/entities/TransactionSetupException.java
+++ src/main/java/beaform/entities/TransactionSetupException.java
@@ -2,36 +2,22 @@ package beaform.entities;
 
 /**
  * This exception indicates a failure in setting up a transaction.
+ *
  * @author Steven Post
  *
  */
 public class TransactionSetupException extends Exception {
 
-	/**
-	 *
-	 */
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * Default constructor.
-	 */
 	public TransactionSetupException() {
 		super();
 	}
 
-	/**
-	 * Constructor.
-	 * @param message the message for this exception.
-	 */
 	public TransactionSetupException(final String message) {
 		super(message);
 	}
 
-	/**
-	 * Constructor.
-	 * @param message the message for this exception
-	 * @param cause the cause of this exception
-	 */
 	public TransactionSetupException(final String message, final Throwable cause) {
 		super(message, cause);
 	}
