d18eebd04b20c9f18eda27fce6acc5d0e1993721
Steven Post <steven.post@persgroep.be>
2016-06-30 22:34:25 +0200

    Remove redundant comments

--- src/main/java/beaform/entities/Formula.java
+++ src/main/java/beaform/entities/Formula.java
@@ -24,99 +24,55 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 @Entity
 public class Formula {
 
-	/** The name of this formula */
 	@Id
 	private String name = "";
-
-	/** The description of this formula */
 	private String description = "";
-
-	/** The total amount of this formula */
 	private String totalAmount = "";
 
-	/** All ingredients that make up this formula */
 	@OneToMany
 	private Map<String, Formula> ingredients = new ConcurrentHashMap<>();
 
-	/** All tags associated with this formula */
 	@OneToMany(fetch=FetchType.EAGER, cascade=CascadeType.PERSIST)
 	private List<FormulaTag> tags = new ArrayList<>();
 
-	/**
-	 * Default constructor, needed by Hibernate.
-	 */
 	public Formula() {
 		// Default constructor for Hibernate.
 	}
 
-	/**
-	 * Constructor.
-	 * @param name The name for this formula
-	 * @param description A description for this formula
-	 * @param totalAmount the total amount
-	 */
 	public Formula(final String name, final String description, final String totalAmount) {
 		this.name = name;
 		this.description = description;
 		this.totalAmount = totalAmount;
 	}
 
-	/**
-	 * @return the name
-	 */
 	public String getName() {
 		return this.name;
 	}
 
-	/**
-	 * @param name the name to set
-	 */
 	public void setName(final String name) {
 		this.name = name;
 	}
 
-	/**
-	 * @return the description
-	 */
 	public String getDescription() {
 		return this.description;
 	}
 
-	/**
-	 * @param description the description to set
-	 */
 	public void setDescription(final String description) {
 		this.description = description;
 	}
 
-	/**
-	 * @return the totalAmount
-	 */
 	public String getTotalAmount() {
 		return this.totalAmount;
 	}
 
-	/**
-	 * Set the total amount in this formula.
-	 * @param totalAmount
-	 */
 	public void setTotalAmount(final String totalAmount) {
 		this.totalAmount = totalAmount;
 	}
 
-	/**
-	 * Add an ingredient to this formula.
-	 * @param ingredient the ingredient to add
-	 * @param amount the amount of this ingredient
-	 */
 	public void addIngredient(final Formula ingredient, final String amount) {
 		this.ingredients.put(ingredient.getName() + "|" + amount, ingredient);
 	}
 
-	/**
-	 * Get a list of ingredients.
-	 * @return a list of ingredients
-	 */
 	public List<Ingredient> getIngredients() {
 		final ArrayList<Ingredient> returnIngredients = new ArrayList<>();
 
@@ -134,39 +90,22 @@ public class Formula {
 		return new Ingredient(formula, tmpAmount);
 	}
 
-	/**
-	 * Delete all ingredients from this formula.
-	 */
 	public void clearIngredients() {
 		this.ingredients.clear();
 	}
 
-	/**
-	 * Add a tag to this formula.
-	 * @param tag the tag to add
-	 */
 	public void addTag(final FormulaTag tag) {
 		this.tags.add(tag);
 	}
 
-	/**
-	 * Get all the tags associated with this formula.
-	 * @return all the tags associated with this formula
-	 */
 	public Iterator<FormulaTag> getTags() {
 		return this.tags.iterator();
 	}
 
-	/**
-	 * Delete all tags from this formula.
-	 */
 	public void clearTags() {
 		this.tags.clear();
 	}
 
-	/**
-	 * @return a list of tags, represented as strings
-	 */
 	public List<String> getTagsAsStrings() {
 		final List<String> retval = new ArrayList<>(this.tags.size());
 		for (final FormulaTag tag : this.tags) {
@@ -175,9 +114,6 @@ public class Formula {
 		return retval;
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
 	public String toString() {
 		final List<String> tagList = getTagsAsStrings();
@@ -189,9 +125,6 @@ public class Formula {
 
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
 	public boolean equals(final Object obj) {
 
@@ -210,9 +143,6 @@ public class Formula {
 		return false;
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
 	@Override
 	public int hashCode() {
 		return new HashCodeBuilder().
