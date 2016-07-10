1681f40e11d876328dc71fb1b79acaa346f8fcc0
Steven Post <redalert.commander@gmail.com>
2016-07-10 13:34:03 +0200

    Mark this class as a utility class

--- beaform-core/src/main/java/beaform/debug/AsyncDebugUtils.java
+++ beaform-core/src/main/java/beaform/debug/AsyncDebugUtils.java
@@ -4,8 +4,13 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public class AsyncDebugUtils {
+public final class AsyncDebugUtils {
+
 	private static final ExecutorService EXECUTOR = Executors.newSingleThreadExecutor();
+
+	private AsyncDebugUtils() {
+		// Utility classes don't need public constructors
+	}
 
 	public static Future<String> clearDb() {
 		return EXECUTOR.submit(new ClearDbTask(), "Done");
