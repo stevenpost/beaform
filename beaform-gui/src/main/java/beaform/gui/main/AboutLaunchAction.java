9e336983450302dec78561449fef4b336f378b7d
Steven Post <redalert.commander@gmail.com>
2016-07-11 21:19:44 +0200

    Move the about dialog a level deeper

--- beaform-gui/src/main/java/beaform/gui/main/AboutLaunchAction.java
+++ beaform-gui/src/main/java/beaform/gui/main/AboutLaunchAction.java
@@ -3,8 +3,6 @@ package beaform.gui.main;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import beaform.gui.About;
-
 /**
  * This action lets one launch the 'about' window.
  *
