8f964856f4bfd665f5ab073151c5df6c5325683d
Steven Post <redalert.commander@gmail.com>
2018-04-01 16:59:13 +0200

    Create an event when creating a formula

--- beaform-core/src/main/java/beaform/commands/CreateNewFormulaCommand.java
+++ beaform-core/src/main/java/beaform/commands/CreateNewFormulaCommand.java
@@ -1,9 +1,15 @@
 package beaform.commands;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import beaform.dao.FormulaDAO;
 import beaform.entities.Formula;
+import beaform.events.Event;
+import beaform.events.FormulaCreatedEvent;
 
 public class CreateNewFormulaCommand implements Command {
+	private static final Logger LOG = LoggerFactory.getLogger(CreateNewFormulaCommand.class);
 
 	private final Formula formula;
 
@@ -13,6 +19,8 @@ public class CreateNewFormulaCommand implements Command {
 
 	@Override
 	public void execute() {
+		Event create = new FormulaCreatedEvent(this.formula.getName());
+		LOG.debug(create.toEventString());
 		FormulaDAO.addFormula(this.formula);
 	}
 
