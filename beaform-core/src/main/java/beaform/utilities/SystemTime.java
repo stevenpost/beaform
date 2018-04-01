6377d51eb26e0332aaf46be04df9811e5eb83030
Steven Post <redalert.commander@gmail.com>
2018-04-01 15:07:30 +0200

    Remove unused code

--- beaform-core/src/main/java/beaform/utilities/SystemTime.java
+++ beaform-core/src/main/java/beaform/utilities/SystemTime.java
@@ -1,7 +1,5 @@
 package beaform.utilities;
 
-import java.util.Date;
-
 public class SystemTime {
 
 	private static final TimeSource defaultSrc = new TimeSource(){
@@ -20,10 +18,6 @@ public class SystemTime {
 
 	public static long getAsLong() {
 		return getCurrentTimeSource().getSystemTime();
-	}
-
-	public static Date getAsDate() {
-		return new Date(getAsLong());
 	}
 
 	private static TimeSource getCurrentTimeSource() {
