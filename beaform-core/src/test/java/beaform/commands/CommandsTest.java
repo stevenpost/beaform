71cd4f6c7c6b4f2d9e9d6705eeec33a768d78603
Steven Post <redalert.commander@gmail.com>
2018-04-08 16:44:28 +0200

    Fix test

--- beaform-core/src/test/java/beaform/commands/CommandsTest.java
+++ beaform-core/src/test/java/beaform/commands/CommandsTest.java
@@ -51,7 +51,7 @@ public class CommandsTest {
 		Command cmd2 = new CreateNewFormulaCommand(formulaToPersist2, this.errorDisplay);
 		cmd.execute();
 
-		String startOfErrorMessage = "A formula with the name testName already seems to exist: ";
+		String startOfErrorMessage = "A formula with the name testName already seems to exist:\n";
 		String assertFailedMessage = "I was expecting a different error message.";
 		assertTrue(assertFailedMessage, this.errorDisplay.getLastError().startsWith(startOfErrorMessage));
 	}
